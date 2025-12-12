84cf4043a8a82bef4303648e5635605ca21665f5
dapuncomputer <dapuncomputer@gmail.com>
2025-12-12 08:50:50 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// Toggle class active Untuk hamburger menu
+const navbarNav = document.querySelector ('.navbar-nav');
+// Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Togle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// Togle Class Active Untuk Shopping Cart
+const ShoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = () => {
+    ShoppingCart.classList.toggle('active');
+}
+
+// Klik di luar elemen
+const hm = document.querySelector ('#hamburger-menu');
+const sb = document.querySelector ('#search-button');
+const sc = document.querySelector ('#shopping-cart-button');
+
+document.addEventListener('click',function(e) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+
+    if(!sc.contains(e.target) && !ShoppingCart.contains(e.target)) {
+        ShoppingCart.classList.remove('active');
+    }
+});
+
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+// Klik Tombol Close Modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+
+// Klik luar Modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal)
+        itemDetailModal.style.display = 'none';
+};
